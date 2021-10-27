daf55c6e7a50d1b8c33971d30e863445d28510c8
KimAeri <52875499+GHkar@users.noreply.github.com>
2021-10-27 17:20:36 +0900

    Update rtx.py

--- rtx.py
+++ rtx.py
@@ -1,5 +1,5 @@
-# ▒~Z~T청▒~U~\ ▒~J▒▒~^~\▒~^▒▒~E~X 리▒~J▒▒~J▒ ▒| ~@▒~^▒
-# SubNode ▒~Z▒
+# 요청한 트랜잭션 리스트 저장
+# SubNode 용
 
 import pymongo
 from pymongo import MongoClient
@@ -9,19 +9,19 @@ import time
 import datetime
 import numpy as np
 
-#=======================================▒~@▒~H~X========================================#
-# ▒| ~U▒~\▒~K~]
+#=======================================변수========================================#
+# 정규식
 pattern_endTime = re.compile(
     r"(?P<date>\d+[-]\d+[-]\d+)T(?P<time>\d+[:]\d+[:]\d+[.]\d+)Z\sSuccessfully\sreconstructed\sblock\s(?P<hash>\w+)\swith\s(?P<prefilled>\d+)\stxn\sprefilled\,\s\d+\stxn\sfrom\smempool\s\(incl\sat\sleast\s\d+\sfrom\sextra\spool\)\sand\s(?P<rtx>\d+)\stxn\srequested")
 pattern_getHeight = re.compile(
     r"UpdateTip[:]\snew\sbest\=(?P<hash>\w+)\sheight[=](?P<height>\d+)\sversion\=\w+\slog2[_]work\=\d+[.]\d+\stx\=\d+\sdate\=\'\d+[-]\d+[-]\d+T\d+[:]\d+[:]\d+Z\'\sprogress\=\d+[.]\d+\scache\=\d+[.]\d+MiB\(\d+txo\)")
 
- # 컬▒| ~I▒~E~X▒~W~P ▒| ~@▒~^▒▒~U|  ▒~U~L ▒~B▒▒~Z▒▒~U|  json 배▒~W▒
+ # 컬렉션에 저장할 때 사용할 json 배열
 block = {}
 
-#=======================================▒~U▒▒~H~X========================================#
+#=======================================함수========================================#
 
-# 몽▒|  db▒~W~P ▒~W▒결
+# 몽고 db에 연결
 def connect_monogoDB():
     myclient = pymongo.MongoClient("mongodb://210.125.31.240:27017/")
     mydb = myclient['prefilledtxn']
@@ -29,16 +29,16 @@ def connect_monogoDB():
 
     return collection
 
-# ▒~T~T▒~D그 ▒~L~L▒~]▒ ▒~W▒▒|  ▒~]▒기
+# 디버그 파일 열고 읽기
 def open_debug_log(collection):
     global block
 
     passbool = False
     #
     file = "debug.log"
-    f = open(file, "r") # ▒~L~L▒~]▒ ▒~W▒기
+    f = open(file, "r") # 파일 열기
     f.seek(5)
-    # ▒~U~D▒~Z~T ▒~@▒~H~X
+    # 필요 변수
     count = 0
 
     while True:
@@ -56,12 +56,12 @@ def open_debug_log(collection):
                 cmpct_hash = find_line.group('hash')
                 rtx = find_line.group('rtx')
                 passbool = True
-                # if time_1 > datetime.datetime(2021,6,22,15,32,43) and time_1 < datetime.datetime(2021,6,24,5,46,9): # 6▒~[~T 23▒~]▒
+                # if time_1 > datetime.datetime(2021,6,22,15,32,43) and time_1 < datetime.datetime(2021,6,24,5,46,9): # 6월 23일
                 #     passbool = True
                 # if time_1 > datetime.datetime(2021,6,24,5,46,8):
                 #     print_result(count)
                 #     break
-                #if time_1 > datetime.datetime(2021,6,27,14,3,50) and time_1 < datetime.datetime(2021,6,29,10,51,8): # 6▒~[~T 27▒~]▒
+                #if time_1 > datetime.datetime(2021,6,27,14,3,50) and time_1 < datetime.datetime(2021,6,29,10,51,8): # 6월 27일
 
             if pattern_getHeight.search(line) and passbool:
                 find_line = pattern_getHeight.search(line)
@@ -81,30 +81,28 @@ def open_debug_log(collection):
 
     f.close()
 
-# 몽▒|  db▒~W~P ▒| ~@▒~^▒
+# 몽고 db에 저장
 def save_mongo_db(collection):
     global block
 
     collection.insert_one(block)
 
 #
-# # ▒~K~\▒~D ▒~X~U▒~@▒~Y~X
+# # 시간 형변환
 # def change_dateTime(date, time):
 #     dateTime = datetime.datetime.strptime(date + " " + time, '%Y-%m-%d %H:%M:%S.%f')
 #
 #     return dateTime
 
-# 결과를 ▒~\▒| ▒
+# 결과를 출력
 def print_result(count):
     if count == 0:
-        print("모▒~S|  ▒~M▒▒~]▒▒~D▒▒~@ DB▒~W~P ▒~^~E▒| ▒ ▒~Y~D▒~L▒~P~\ ▒~C~A▒~C~\▒~^~E▒~K~H▒~K▒.")
+        print("모든 데이터가 DB에 입력 완료된 상태입니다.")
     else :
-        print("▒~] " + str(count) + "▒~\를 DB▒~W~P ▒| ~@▒~^▒▒~U~X▒~X~@▒~J▒▒~K~H▒~K▒.")
+        print("총 " + str(count) + "개를 DB에 저장하였습니다.")
 
 #=======================================본문========================================#
 
 collection = connect_monogoDB()
 open_debug_log(collection)
-
-
 
